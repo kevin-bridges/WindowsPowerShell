d31d13bbfa7e366b0de8678ef6d9206040d197bf
Kevin Bridges <kevin.b.bridges@gmail.com>
2023-04-12 18:16:16 -0500

    Update Update-IISBindings.ps1

--- Scripts/Update-IISBindings.ps1
+++ Scripts/Update-IISBindings.ps1
@@ -1,3 +1,36 @@
+<#
+.SYNOPSIS
+Updates the binding information for a website in IIS on one or more remote servers.
+
+.DESCRIPTION
+This script updates the binding information for a website in IIS on one or more remote servers. The script accepts the following parameters:
+- SiteName: The name of the website to update.
+- IPAddress: The new IP address to use for the binding.
+- Port: The new port to use for the binding.
+- ComputerName: An optional list of remote server names to update. If not specified, the script will run locally.
+
+.PARAMETER SiteName
+The name of the website to update.
+
+.PARAMETER IPAddress
+The new IP address to use for the binding.
+
+.PARAMETER Port
+The new port to use for the binding.
+
+.PARAMETER ComputerName
+An optional list of remote server names to update. If not specified, the script will run locally.
+
+.EXAMPLE
+Update-IISBinding -SiteName "MySite" -IPAddress "10.0.0.1" -Port "80" -ComputerName "Server01", "Server02"
+
+This example updates the binding information for the website named "MySite" to use the IP address "10.0.0.1" and port "80" on the servers "Server01" and "Server02".
+
+.NOTES
+This script requires the WebAdministration module to be installed on the server(s) being updated.
+
+#>
+
 param(
     [string]$SiteName,
     [string]$IPAddress,
